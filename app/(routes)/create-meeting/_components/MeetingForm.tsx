3ef3e0cc63f961edf5edc37dff58d3df0c599505
Kevin Mbitkebeyo <mbitkebeyokevin@gmail.com>
2024-05-05 21:47:32 +0100

    creating form to add meeting

--- app/(routes)/create-meeting/_components/MeetingForm.tsx
+++ app/(routes)/create-meeting/_components/MeetingForm.tsx
@@ -1,12 +1,47 @@
-import { ChevronLeft } from 'lucide-react'
-import React from 'react'
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Input } from "@/components/ui/input";
+import { ChevronLeft } from "lucide-react";
+import React from "react";
 
 const MeetingForm = () => {
   return (
-    <div className='p-4'>
-        <h2><ChevronLeft/> Cancel</h2>
-    </div>
-  )
-}
+    <div className="p-4">
+      <h2>
+        <ChevronLeft /> Cancel
+      </h2>
+      <div className="mt-4">
+        <h2 className="font-bold text-2xl my-4">Create new Event</h2>
+        <hr />
+      </div>
+      <div className="flex flex-col gap-3 my-4">
+        <h2 className="font-bold">Event Name *</h2>
+        <Input placeholder="Name of your meeting" />
+        <h2>Duration *</h2>
 
-export default MeetingForm
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="outline" className="max-w-40">
+              30 Min
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent>
+       
+            <DropdownMenuItem>15 Min</DropdownMenuItem>
+            <DropdownMenuItem>30 Min</DropdownMenuItem>
+            <DropdownMenuItem>45 Min</DropdownMenuItem>
+            <DropdownMenuItem>60 Min</DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </div>
+    </div>
+  );
+};
+export default MeetingForm;
--- app/(routes)/create-meeting/page.tsx
+++ app/(routes)/create-meeting/page.tsx
@@ -1,17 +1,15 @@
-import React from 'react'
-import MeetingForm from './_components/MeetingForm'
+import React from "react";
+import MeetingForm from "./_components/MeetingForm";
 
 const CreateMeetingPage = () => {
   return (
-    <div className='grid grid-cols-1 md:grid-cols-3'>
-        <div>
-<MeetingForm/>
-        </div>
-        <div className='md:col-span-2'>
-
-        </div>
+    <div className="grid grid-cols-1 md:grid-cols-3">
+      <div className="shadow-md border h-screen">
+        <MeetingForm />
+      </div>
+      <div className="md:col-span-2"></div>
     </div>
-  )
-}
+  );
+};
 
-export default CreateMeetingPage
+export default CreateMeetingPage;
